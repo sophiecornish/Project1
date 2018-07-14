4806b309e23b91ca83436c0ad398f7b3096bb60c
Sophie Cornish <sophieahcornish@gmail.com>
2018-07-14 11:54:47 +0100

    movement, disappear on click, timer & reset button

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
 
 
-  ////TRUMPS CLICK FUNCTION ////
+  ////   TRUMPS CLICK FUNCTION   ////
 
   const $trumps = $('.trump');
   console.log($trumps);
@@ -9,36 +9,44 @@ $(document).ready(() => {
 
   $trumps.on('click', click);
 
-  function click (e) {
-    $(e.target).hide(500);
 
+  function click (e) {
+    $(e.target).hide(400);
   }
 
-  // ////TIMER//////
+  // ////   TIMER   //////
 
 
   const $timer = $('.timer');
   let countDownValue = $timer.html();
   const $start = $('#start');
+  let timerRunning = false;
+
 
   $start.on('click', function() {
+
     const countdown = setInterval(() => {
       countDownValue --;
       $timer.html(countDownValue);
+      timerRunning = true;
       if (countDownValue ===0) {
         clearInterval(countdown);
+        timerRunning = false;
       }
     }, 1000);
+
+
 
 
     ////    RESET     ////
 
     const $reset = $('#reset');
 
-    $reset.on('click', function (){
+    $reset.on('click', function() {
       clearInterval(countdown);
-      $timer.html = 10;
-
+      countDownValue = 10;
+      $timer.html(10);
+      timerRunning = false;
 
     });
 
