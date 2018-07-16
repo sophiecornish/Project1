683cedc062e9a7c9ffc1eea6814a875948b9e02b
Sophie Cornish <sophieahcornish@gmail.com>
2018-07-16 16:12:47 +0100

    re-coded in javascript

--- app.js
+++ app.js
@@ -1,98 +1,123 @@
 $(document).ready(() => {
 
+// -------------------generate trumps function ---------------------------//
 
-  ///////// generate random animation ///////////////
+  const trumps = $('.trumps');
+
+  const images = [ './images/trumpOne.png', './images/trumpTwo.png', './images/trumpThree.png', './images/trumpFour.png', './images/trumpFive.png'   ];
+
+  const speeds = ['10', '12', '15'];
 
   const animations = ['circle', 'rotate', 'bounce'];
-  const animation = animations[Math.floor(Math.random()*animations.length)];
-  console.log(animation);
 
-  const trumps = ['trumpOne', 'trumpTwo', 'trumpThree', 'trumpFour', 'trumpFive'];
+  function addTrumps(numberOfTrumps) {
+    for(let i = 0; i < numberOfTrumps; i++) {
+      const randomImage = images[Math.floor(Math.random()*images.length)];
 
-  const trump = trumps[Math.floor(Math.random()*trumps.length)];
-  console.log(trump);
+      const randomAnimation = animations[Math.floor(Math.random()*animations.length)];
+      console.log(randomAnimation);
 
-  function animate() {
-    console.log($('.trump').style);
-    $('.trump').css({animationName: animation});
-  }
-
-  animate();
+      const randomSpeed =
+      speeds[Math.floor(Math.random()*speeds.length)];
+      console.log(randomSpeed);
 
 
 
-
-
-
-  ////   TRUMPS CLICK FUNCTION   ////
-
-  const $trumps = $('.trump');
-  console.log($trumps);
-  const audio = document.querySelector('audio');
-
-
-  $trumps.on('click', click);
-
-
-  function click (e) {
-    $(e.target).hide(400);
-    audio.src = `./sounds/${e.target.id}.mp3`;
-
-
-    audio.play();
-  }
-
-  // ////   TIMER   //////
-
-
-  const $timer = $('.timer');
-  let countDownValue = $timer.html();
-  const $start = $('#start');
-  let timerRunning = false;
-  var clickDisabled = false;
-
-
-
-  $trumps.on('click', function() {
-
-    if (clickDisabled) {
-      return;
-
-    }  else {
-
-      const countdown = setInterval(() => {
-
-        clickDisabled = true;
-        countDownValue --;
-        $timer.html(countDownValue);
-        timerRunning = true;
-        if (countDownValue ===0) {
-          clearInterval(countdown);
-
-
-
-          timerRunning = false;
-        }
-      }, 1000);
+      const newTrump = document.createElement('div');
+      newTrump.className = 'trump';
+      newTrump.style.backgroundImage = `url("${randomImage}")`;
+      newTrump.style.animationName = randomAnimation;
+      newTrump.style.animationDuration = randomSpeed;
+      trumps.append(newTrump);
     }
 
 
+  }
+  addTrumps(8);
+
+  const audio = document.querySelector('audio');
+  const noises = ['trumpOne', 'trumpTwo', 'trumpThree', 'trumpFour', 'trumpFive'];
 
 
-    ////    RESET     ////
 
-    const $reset = $('#reset');
 
-    $reset.on('click', function() {
-      clearInterval(countdown);
-      countDownValue = 10;
-      $timer.html(10);
-      timerRunning = false;
+  trumps.on('click', '.trump', function(){
+    const randomNoise = noises[Math.floor(Math.random()*noises.length)];
+    console.log(randomNoise);
+    $(event.target).hide(400);
+    audio.src = `./sounds/${randomNoise}.mp3`;
+    audio.play();
+  });
 
-    });
+});
 
+
+
+//------------------- TRUMPS CLICK FUNCTION ----------------------//
+
+// const $trumps = $('.trumps');
+
+
+
+// $trumps.on('click', click());
+
+
+
+// function click(e) {
+//   $(e.target).hide(400);
+//   audio.src = `./sounds/${randomNoise}.mp3`;
+//   console.log('hi');
+
+
+
+// }
+
+//-------------------TIMER ----------------------//
+
+
+const $timer = $('.timer');
+let countDownValue = $timer.html();
+const $start = $('#start');
+let timerRunning = false;
+var clickDisabled = false;
+
+
+
+$trumps.on('click', function() {
+
+  if (clickDisabled) {
+    return;
+
+  }  else {
+
+    const countdown = setInterval(() => {
+
+      clickDisabled = true;
+      countDownValue --;
+      $timer.html(countDownValue);
+      timerRunning = true;
+      if (countDownValue ===0) {
+        clearInterval(countdown);
+        timerRunning = false;
+      }
+    }, 1000);
+  }
+
+
+
+
+  //------------------------ RESET --------------------------//
+
+  const $reset = $('#reset');
+
+  $reset.on('click', function() {
+    clearInterval(countdown);
+    countDownValue = 10;
+    $timer.html(10);
+    timerRunning = false;
 
   });
+
 
 
 
